74764f3af500f898d2c53087b7b9628be9f4af0a
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-17 14:20:39 +0100

    Add API versioning and error handling to main router

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,39 +2,20 @@
  * @file Defines the main router for the application.
  * @module router
  * @author Fredrik Svensson
+ * @version 0.1.0
  * @since 0.1.0
  */
 
 import express from 'express'
+import { router as v1Router } from '.api/v1/router.js'
 
 export const router = express.Router()
 
-/**
- * @openapi
- * /:
- *   get:
- *     summary: Get API status
- *     description: Returns the status of the API.
- *     tags:
- *      - Status
- *     responses:
- *       200:
- *         description: API is up and running.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: Welcome to the NjordBreeze API. Use our endpoints to interact with weather data.
- *                 documentation:
- *                   type: string
- *                   example: https://cscloud8-57.lnu.se/njordbreeze/docs
- */
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the NjordBreeze API. Use our endpoints to interact with weather data.',
-    documentation: 'https://cscloud8-57.lnu.se/njordbreeze/docs'
-  })
+router.use('/api/v1', v1Router)
+
+//Error handling
+router.use('*', (req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
 })
