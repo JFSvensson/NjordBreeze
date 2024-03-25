29d556b5d09cb7af7c69a2b1f46a2666878b8334
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-25 16:52:09 +0100

    Add login functionality to authController.js

--- src/controllers/api/v1/authController.js
+++ src/controllers/api/v1/authController.js
@@ -5,6 +5,8 @@
  * @since 0.1.0
  */
 
+import jwt from 'jsonwebtoken'
+import fs from 'fs'
 import createError from 'http-errors'
 import { AuthService } from '../../../services/api/v1/authService.js'
 /**
@@ -40,6 +42,46 @@ export class AuthController {
         err.message = 'The request cannot or will not be processed due to something that is perceived to be a client error (for example validation error).'
         err.cause = error
       }
+
+      next(err)
+    }
+  }
+
+  /**
+   * Authenticates a user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async login (req, res, next) {
+    try {
+      const user = await this.authService.authenticateUser(req.body.username, req.body.passphrase)
+      const payload = {
+        sub: user._id,
+        given_name: user.firstName,
+        family_name: user.lastName,
+        email: user.email
+      }
+
+      // WARNING! PRODUCTION CODE SHOULD NOT CONTAIN THIS LINE! TODO: REMOVE THIS LINE!
+      process.env.ACCESS_TOKEN_SECRET = fs.readFileSync('./private.pem', 'utf8')
+      const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+        algorithm: 'RS256',
+        expiresIn: process.env.ACCESS_TOKEN_LIFE
+      })
+
+      res
+        .status(200)
+        .json({
+          access_token: accessToken
+        })
+    } catch (error) {
+      // Authentication failed.
+      console.error(error)
+      const err = createError(401)
+      err.message = 'Credentials invalid or not provided.'
+      err.cause = error
 
       next(err)
     }
