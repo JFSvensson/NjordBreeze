d19f45c9aafbe82aff13555db25d5548d5acedd9
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-25 14:14:10 +0100

    Add user registration functionality

--- src/controllers/api/v1/authController.js
+++ src/controllers/api/v1/authController.js
@@ -5,18 +5,48 @@
  * @since 0.1.0
  */
 
+import createError from 'http-errors'
+import { User } from '../../../models/user.js'
 
 /**
  * Handles requests regarding authorization.
  */
 export class AuthController {
   /**
-   * Creates a new user.
+   * Register and create a new user.
    * @param {Request} req - The request object.
    * @param {Response} res - The response object.
    * @param {NextFunction} next - The next middleware function.
    */
   async register(req, res, next) {
-    res.send('User created')
+    try {
+      const user = new User({
+        username: req.body.username,
+        passphrase: req.body.passphrase,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+      })
+      await user.save()
+
+      res
+        .status(201)
+        .json({ id: user.id })
+    } catch (error) {
+      let err = error
+
+      if (err.code === 11000) {
+        // Duplicated keys.
+        err = createError(409)
+        err.message = 'The username and/or email address is already registered.'
+      } else if (error.name === 'ValidationError') {
+        // Validation error(s).
+        err = createError(400)
+        err.message = 'The request cannot or will not be processed due to something that is perceived to be a client error (for example validation error).'
+        err.cause = error
+      }
+
+      next(err)
+    }
   }
 }
