ee1d8db471dc26ea95d4c5074f08135502199c24
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-25 14:30:48 +0100

    Add authorization service for creating users

---
+++ src/services/api/v1/authService.js
@@ -0,0 +1,17 @@
+/**
+ * @file Defines the authorization service for the application.
+ * @module service
+ * @author Fredrik Svensson
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
+import { User } from '../../../models/user.js'
+
+export class AuthService {
+  async createUser(userData) {
+    const user = new User(userData)
+    await user.save()
+    return user
+  }
+}
