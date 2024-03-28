da1c878c35b501ce0f5ca65194ac7cea28d0db21
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-28 21:25:47 +0100

    Update user service methods to not return sensitive information

--- src/services/api/v1/usersService.js
+++ src/services/api/v1/usersService.js
@@ -10,17 +10,17 @@ import { User } from '../../../models/user.js'
 
 export class UsersService {
   async getUserById(id) {
-    const user = await User.findById(id)
+    const user = await User.findById(id).select('-username -passphrase')
     return user
   }
 
   async updateUser(id, data) {
     const user = await User.findByIdAndUpdate(id, data, { new: true, runValidators: true })
-    return user
+    return { message: 'User updated successfully' }
   }
 
   async deleteUser(id) {
     const user = await User.findByIdAndDelete(id)
-    return user
+    return { message: 'User deleted successfully' }
   }
 }
