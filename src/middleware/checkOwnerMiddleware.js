b450587b3e24982d522c797fe31a4a0e0209ce64
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-30 12:01:17 +0100

    Add checkOwnerStation middleware to verify station ownership

--- src/middleware/checkOwnerMiddleware.js
+++ src/middleware/checkOwnerMiddleware.js
@@ -6,6 +6,8 @@
  * @since 0.1.0
  */
 
+import { Station } from '../models/station.js'
+
 export class CheckOwnerMiddleware {
 
   /**
@@ -21,6 +23,24 @@ export class CheckOwnerMiddleware {
     const userIdFromParams = req.params.id
     if (userIdFromToken !== userIdFromParams) {
       return res.status(403).json({ message: 'Forbidden: You can only access your own data' })
+    }
+    next()
+  }
+
+  /**
+   * Middleware to check if the request is authorized to modify a station.
+   * @param {Request} req - The request object.
+   * @param {Response} res - The response object.
+   * @param {NextFunction} next - The next middleware function.
+   * @returns {void}
+   * @throws {Error} Throws an error if the user is not the owner of the station.
+   */
+  async checkOwnerStation(req, res, next) {
+    const userIdFromToken = req.user.sub
+    const stationId = req.params.id
+    const station = await Station.findById(stationId)
+    if (!station || userIdFromToken.toString() !== station.owner.toString()) {
+      return res.status(403).json({ message: 'Forbidden: You can only modify your own station' })
     }
     next()
   }
