c19993291193ca1b42087e2649cadf7e747897e2
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-04 15:12:45 +0200

    Add HATEOAS links to API status endpoint

--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -12,20 +12,23 @@ import { router as weatherRouter } from './weatherRouter.js'
 import { router as stationsRouter } from './stationsRouter.js'
 import { router as usersRouter } from './usersRouter.js'
 import { router as webhooksRouter } from './webhooksRouter.js'
+import { HateoasMiddleware } from '../../../middleware/hateoasMiddleware.js'
 
 export const router = express.Router()
+
+const hateoas = new HateoasMiddleware()
 
 /**
  * @openapi
  * /:
  *   get:
  *     summary: Get API status
- *     description: Returns the status of the API.
+ *     description: Returns the status of the API and provides dynamical links to other endpoints.
  *     tags:
  *      - Status
  *     responses:
  *       200:
- *         description: API is up and running.
+ *         description: API is up and running. 
  *         content:
  *           application/json:
  *             schema:
@@ -37,14 +40,42 @@ export const router = express.Router()
  *                 documentation:
  *                   type: string
  *                   example: https://cscloud8-57.lnu.se/njordbreeze/docs
+ *                 _links:
+ *                   type: object
+ *                   properties:
+ *                     self:
+ *                       type: string
+ *                       description: The current endpoint.
+ *                       example: /
+ *                     auth:
+ *                       type: string
+ *                       description: The authentication endpoint.
+ *                       example: /auth
+ *                     weather:
+ *                       type: string
+ *                       description: The weather data endpoint.
+ *                       example: /weather
+ *                     stations:
+ *                       type: string
+ *                       description: The weather station data endpoint.
+ *                       example: /stations
+ *                     users:
+ *                       type: string
+ *                       description: The user data endpoint.
+ *                       example: /users
+ *                     webhooks:
+ *                       type: string
+ *                       example: /webhooks
  */
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the NjordBreeze API. Use our endpoints to interact with weather data.',
-    documentation: 'https://cscloud8-57.lnu.se/njordbreeze/docs'
-    // TODO: Implement HATEOAS links
-  })
-})
+router.get(
+  '/',
+  hateoas.addLinks,
+  (req, res) => {
+    res.json({
+      message: 'Welcome to the NjordBreeze API. Use our endpoints to interact with weather data.',
+    })
+  }
+)
 
 router.use('/auth', authRouter)
 router.use('/weather', weatherRouter)
